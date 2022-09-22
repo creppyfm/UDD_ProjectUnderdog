4a48e5ac78c6ad2cc4ad156ca4bd224b015a7c50
creppyfm <97473588+creppyfm@users.noreply.github.com>
2022-09-22 15:20:10 -0400

    problem five done

--- src/PracticeProblems/FIVE_fifteenLettersNoEA.java
+++ src/PracticeProblems/FIVE_fifteenLettersNoEA.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 import java.nio.file.Paths;
 import java.io.FileNotFoundException;
 
-public class TWO_containsXYZ {
+public class FIVE_fifteenLettersNoEA {
     public static void main(String[] args) {
         try (Scanner reader = new Scanner(Paths.get("sowpods.txt"))) {
             while (reader.hasNextLine()) {
                 String line = reader.nextLine();
-                if (line.contains("X") &&
-                    line.contains("Y") &&
-                    line.contains("Z")) {
-                        System.out.println(line);
+                if (line.length() >= 15 &&
+                    !line.contains("E") &&
+                    !line.contains("A")) {
+                    System.out.println(line);
                 }
             }
         } catch (IOException e) {
             System.out.println("See Error Below:");
             e.printStackTrace();
-            throw new RuntimeException(e);
+            throw new RuntimeException();
         }
     }
 }
