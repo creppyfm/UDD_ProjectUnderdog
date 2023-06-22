769cea921bd453437e5edb2090d37dba5fb12048
creppyfm <97473588+creppyfm@users.noreply.github.com>
2023-06-22 15:03:07 -0400

    49 - GroupAnagrams: Solution

--- src/main/java/LC/_49_GroupAnagrams.java
+++ src/main/java/LC/_49_GroupAnagrams.java
@@ -1,17 +1,43 @@
 package LC;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class _49_GroupAnagrams {
     public static void main(String[] args) {
-
+        String[] strs = { "eat", "ate", "tan", "nat", "bat", "tea" };
+        List<List<String>> solution = groupAnagrams(strs);
+        System.out.println(solution);
     }
 
-    public List<List<String>> groupAnagrams (String[] strs) {
-        List<List<String>> returnList = new ArrayList<>();
-        if (strs.length == 0) return returnList;
-
-        return returnList;
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> solution = new ArrayList<>();
+        if (strs.length == 0) return solution;
+        Map<String, List<String>> hashToAnagrams = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            char[] wordChars = strs[i].toCharArray();
+            int[] hashArr = new int[26];
+            for (char c : wordChars) {
+                hashArr[c - 'a']++;
+            }
+            String hash = Arrays.toString(hashArr);
+            List<String> vals;
+            if (hashToAnagrams.get(hash) != null) {
+                vals = hashToAnagrams.get(hash);
+            } else {
+                vals = new ArrayList<>();
+            }
+            vals.add(strs[i]);
+            hashToAnagrams.put(hash, vals);
+        }
+        solution.addAll(hashToAnagrams.values());
+        return solution;
     }
 }
+
+
+/*
+    Given an array of strings strs, group the anagrams together.
+    You can return the answer in any order.
+    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+    typically using all the original letters exactly once.
+* */
